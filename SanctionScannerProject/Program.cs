625811de5cc50bb2b446d5d2d777e62b45e49d95
gurkankir5501 <gurkan.kir@bil.omu.edu.tr>
2021-11-03 13:24:58 +0300

    first process

--- SanctionScannerProject/Program.cs
+++ SanctionScannerProject/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+// Manage nuget paket yöneticisinden eklemeliyiz.
 using HtmlAgilityPack;
 
 namespace SanctionScannerProject
